220397faf502cf72144f0f3996ea17aa2f202e8b
ArcticZeroo <spencer@artix.cc>
2017-10-12 19:36:36 -0400

    import the homepage, and use page routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './util/TextUtil.dart';
+import './pages/home.dart';
 
 void main() {
   runApp(new MsuHelperApp());
@@ -11,37 +12,15 @@ class MsuHelperApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      color: Colors.blue,
       title: APP_TITLE,
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: new HelperHomepage(),
-    );
-  }
-}
-
-class HelperHomepage extends StatefulWidget {
-  createState() => new HelperHomepageState();
-}
-
-class HelperHomepageState extends State<HelperHomepage> {
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        centerTitle: true,
-        title: TextUtil.getAppBarTitle('Home')
-      ),
-      body: new Center(
-        child: new Text('Hello world!'),
-      ),
+      routes: {
+        '/': (BuildContext context) => new HelperHomepage(),
+        '/foodtruck': (BuildContext context) => new HelperHomepage()
+      }
     );
   }
 }
