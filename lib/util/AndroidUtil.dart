c2225d79abed4505e6f65858495ffcb9cdaf259e
ArcticZeroo <spencer@artix.cc>
2017-10-16 15:29:29 -0400

    Don't crash iOS in AndroidUtil.openMaps

--- lib/util/AndroidUtil.dart
+++ lib/util/AndroidUtil.dart
@@ -1,9 +1,14 @@
 import 'dart:async';
+import 'dart:io' show Platform;
 
 import 'package:android_intent/android_intent.dart';
 
 class AndroidUtil {
   static Future openMaps(String query) async {
+    if (!Platform.isAndroid) {
+      return;
+    }
+
     AndroidIntent androidIntent = new AndroidIntent(
         action: 'action_view',
         data: 'https://www.google.com/maps/search/?api=1&query=${query.split(' ').join('+')}'
