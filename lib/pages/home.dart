a831dfd3c0e5c85cafb49dccfc75a90ffeece507
ArcticZeroo <spencer@artix.cc>
2017-10-16 15:33:01 -0400

    Turn truck view into a set of ListTiles

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,6 +3,9 @@ import 'dart:io' show Platform;
 
 import 'package:flutter/material.dart';
 import 'package:android_intent/android_intent.dart';
+import 'package:msu_helper/util/AndroidUtil.dart';
+import 'package:msu_helper/util/DateUtil.dart';
+import 'package:msu_helper/util/WidgetUtil.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../util/TextUtil.dart';
@@ -32,65 +35,90 @@ class _HomepageState extends State<Homepage> {
         DateTime now = new DateTime.now();
 
         stops.addAll(truckResponse.stops);
-        stops.retainWhere((s) => s.start.day == now.day);
+        stops.retainWhere((s) => !s.isCancelled && s.start.day == now.day);
 
         List<Widget> truckWidgets = new List();
+        List<Widget> truckBadWidgets = new List();
 
         if (stops.length > 0) {
-          Duration timeUntil = (stops.length > 0) ? new DateTime.now().difference(stops[0].start) : new Duration(days: -1);
+          DateTime now = new DateTime.now();
 
-          if (timeUntil.inHours <= 3 && !timeUntil.isNegative) {
-            String truckText = 'The food truck will be at ${stops[0].location} in ${timeUntil.inHours}h ${timeUntil.inMinutes % 60}m.';
+          for (FoodTruckStop stop in stops) {
+            Widget leading = (Platform.isAndroid)
+                ? new Icon(Icons.location_on)
+                : new Icon(Icons.timer);
 
-            if (Platform.isAndroid) {
-              truckWidgets.add(new Column(
-                children: <Widget>[
-                  new Text(truckText),
-                  new FlatButton(
-                    child: new Row(
-                        children: <Widget>[
-                          new Icon(Icons.location_on),
-                          new Text('View in Maps')
-                        ]
-                    ),
-                    onPressed: () async {
-                      AndroidIntent androidIntent = new AndroidIntent(
-                          action: 'action_view',
-                          data: 'https://www.google.com/maps/search/?api=1&query=${stops[0].location.split(' ').join('+')}'
-                      );
+            Widget title;
 
-                      await androidIntent.launch();
-                    },
-                  )
-                ],
+            Duration timeUntil = stop.start.difference(now);
+
+            // Truck is gone
+            if (timeUntil.isNegative || stop.end.isBefore(now)) {
+              truckBadWidgets.add(new ListTile(
+                leading: new Icon(Icons.mood_bad),
+                title: new Text('It left ${stop.location} at ${DateUtil.toTimeString(stop.end)}.'),
               ));
-            } else {
-              truckWidgets.add(new Text(truckText));
+              continue;
             }
-          } else {
-            truckWidgets.add(new Text(truckResponse.toString()));
+            // Truck is here now
+            else{
+              if (stop.start.isBefore(now) && stop.end.isAfter(now)) {
+                title = new Text('It is currently at ${stop.location} until ${DateUtil.toTimeString(stop.end)}');
+              } else {
+                // Truck will be here soon
+                title = new Text('It will be at ${stop.location} from ${DateUtil.toTimeString(stop.start)} until ${DateUtil.toTimeString(stop.end)} (${timeUntil.inHours}h ${timeUntil.inMinutes % 60}m from now)');
+              }
+
+              if (Platform.isAndroid) {
+                truckWidgets.add(new ListTile(
+                  leading: leading,
+                  title: title,
+                  onTap: () async {
+                    if (Platform.isAndroid) {
+                      await AndroidUtil.openMaps(stop.location);
+                    }
+                  },
+                ));
+              } else {
+                truckWidgets.add(new ListTile(
+                  leading: leading,
+                  title: title
+                ));
+              }
+            }
           }
 
-          infoWidgetPriorities[Identifiers.FOOD_TRUCK] = computePriority(base: Priorities.FOOD_TRUCK, when: truckResponse.stops[0].start);
+          infoWidgetPriorities[Identifiers.FOOD_TRUCK] = computePriority(base: Priorities.FOOD_TRUCK, when: stops[0].start);
         } else {
-          truckWidgets.add(new Text('The food truck has no stops listed for today.'));
-
           infoWidgetPriorities[Identifiers.FOOD_TRUCK] = Priorities.FOOD_TRUCK * 0.1;
         }
 
-        infoWidgets[Identifiers.FOOD_TRUCK] = new ListTile(
-          leading: new CircleAvatar(child: new Text('🚚'), backgroundColor: Colors.transparent),
-          title: new Text('MSU Food Truck'),
-          subtitle: new Column(children: truckWidgets)
+        List<Widget> cardChildren = <Widget>[
+          new ListTile(
+              leading: new CircleAvatar(child: new Text('🚚'), backgroundColor: Colors.transparent),
+              title: new Text('MSU Food Truck'),
+              subtitle: new Text('The food truck has ${stops.length == 0 ? 'no' : stops.length} stop${stops.length == 1 ? '' : 's'} today.')
+          )
+        ];
+
+        cardChildren.addAll(truckWidgets);
+        cardChildren.addAll(truckBadWidgets);
+
+        infoWidgets[Identifiers.FOOD_TRUCK] = new Container(
+          padding: new EdgeInsets.all(16.0),
+          child: new Column(children: cardChildren)
         );
       });
     } catch (e) {
       setState(() {
-        infoWidgets[Identifiers.FOOD_TRUCK] = new Column(
-            children: <Widget>[
-              new Text('Could not load food truck data: ${e.toString()}'),
-              new FlatButton(onPressed: loadTruckInfo, child: new Text('Retry'))
-            ]
+        infoWidgets[Identifiers.FOOD_TRUCK] = new Container(
+          padding: new EdgeInsets.all(16.0),
+          child: new Column(
+              children: <Widget>[
+                new Text('Could not load food truck data: ${e.toString()}'),
+                new FlatButton(onPressed: loadTruckInfo, child: new Text('Retry'))
+              ]
+          ),
         );
 
         infoWidgetPriorities[Identifiers.FOOD_TRUCK] = 0.0;
@@ -128,10 +156,9 @@ class _HomepageState extends State<Homepage> {
       }
 
       return new ListView(
-        padding: new EdgeInsets.all(16.0),
         scrollDirection: Axis.vertical,
-        children: children,
-        addRepaintBoundaries: false,
+        children: ListTile.divideTiles(tiles: children, color: Colors.black45).toList(),
+        addRepaintBoundaries: false
       );
     }
   }
