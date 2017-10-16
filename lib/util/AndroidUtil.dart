d7578b2e8b27946c80e116f81a00d6ebeaf606e0
ArcticZeroo <spencer@artix.cc>
2017-10-16 12:02:04 -0400

    WidgetUtil is dumb, add an AndroidUtil instead

---
+++ lib/util/AndroidUtil.dart
@@ -0,0 +1,14 @@
+import 'dart:async';
+
+import 'package:android_intent/android_intent.dart';
+
+class AndroidUtil {
+  static Future openMaps(String query) async {
+    AndroidIntent androidIntent = new AndroidIntent(
+        action: 'action_view',
+        data: 'https://www.google.com/maps/search/?api=1&query=${query.split(' ').join('+')}'
+    );
+
+    await androidIntent.launch();
+  }
+}
