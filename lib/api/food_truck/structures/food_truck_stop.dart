3876e5a2615fcb252a84d4bef0d5203ce930cf42
Spencer <spencer@frozor.io>
2018-04-22 18:45:06 -0400

    add FoodTruckStop

---
+++ lib/api/food_truck/structures/food_truck_stop.dart
@@ -0,0 +1,33 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:msu_helper/api/point.dart';
+import 'package:msu_helper/util/UrlUtil.dart';
+
+part './food_truck_stop.g.dart';
+
+@JsonSerializable()
+class FoodTruckStop extends Object with _$FoodTruckStopSerializerMixin {
+  final String place;
+  final String mapsLocation;
+  final Point location;
+  final bool isCancelled;
+  final DateTime start;
+  final DateTime end;
+
+  FoodTruckStop(this.place, this.location, int start, int end)
+      : this.start = DateTime.fromMillisecondsSinceEpoch(start),
+        this.end = DateTime.fromMillisecondsSinceEpoch(end),
+        this.mapsLocation = place.contains('near') ? place.split('near')[1].trim() : place,
+        this.isCancelled = place.toLowerCase().contains('cancel');
+
+  factory FoodTruckStop.fromJson(Map<String, dynamic> json) => _$FoodTruckStopFromJson(json);
+
+  openMaps() {
+    if (location == null || location.isNull) {
+      UrlUtil.openMaps(mapsLocation);
+      return;
+    }
+
+    UrlUtil.openMapsToCoordinates(location.x, location.y);
+  }
+}
+
