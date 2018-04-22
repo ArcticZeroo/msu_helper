a52e389f5d5e0aa4d497d46c52342fcf242c3f68
Spencer <spencer@frozor.io>
2018-04-22 18:43:20 -0400

    add support for custom add times and returning entries in fetch

--- lib/api/timed_cache.dart
+++ lib/api/timed_cache.dart
@@ -5,8 +5,8 @@ class TimedCacheEntry<T> {
   T value;
   int added;
 
-  TimedCacheEntry(this.value) {
-    added = DateTime.now().millisecondsSinceEpoch;
+  TimedCacheEntry(this.value, [int added]) {
+    this.added = added ?? DateTime.now().millisecondsSinceEpoch;
   }
 
   bool isValid(int expireTime) {
@@ -21,31 +21,44 @@ class TimedCacheEntry<T> {
 
 class TimedCache<K, V> {
   final int expireTime;
-  final Future<V> Function(K) _fetch;
+  final Future<dynamic> Function(K) _fetch;
   Map<K, TimedCacheEntry<V>> _cache;
 
   TimedCache(this._fetch, [this.expireTime = 15*60*1000]) {
     _cache = new Map();
   }
 
-  void put(K key, V value) {
-    _cache[key] = new TimedCacheEntry<V>(value);
+  void put(K key, V value, [int added]) {
+    _cache[key] = new TimedCacheEntry<V>(value, added);
   }
 
-  Future<V> get(K key) async {
+  TimedCacheEntry<V> getDirectEntry(K key) {
     if (!_cache.containsKey(key)) {
       return null;
     }
 
-    TimedCacheEntry entry = _cache[key];
+    return _cache[key];
+  }
 
-    if (entry.isValid(expireTime)) {
-      return entry.value;
+  Future<V> get(K key) async {
+    TimedCacheEntry entry;
+    if (_cache.containsKey(key)) {
+      entry = _cache[key];
+
+      if (entry.isValid(expireTime)) {
+        return entry.value;
+      }
     }
 
-    V value = await _fetch(key);
+    var value = await _fetch(key);
 
-    entry.update(value);
+    if (value is TimedCacheEntry<V>) {
+      _cache[key] = value;
+    } else if (value is V) {
+      put(key, value);
+    } else {
+      throw new TypeError();
+    }
 
     return value;
   }
