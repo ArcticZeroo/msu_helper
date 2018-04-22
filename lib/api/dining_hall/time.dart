45afd062faf401b33befda03e76f34029128ab9f
Spencer <spencer@frozor.io>
2018-04-22 00:02:46 -0400

    add MenuDate

---
+++ lib/api/dining_hall/time.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:msu_helper/api/dining_hall/structures/dining_hall.dart';
+import 'package:msu_helper/api/dining_hall/structures/dining_hall_hours.dart';
+import 'package:msu_helper/util/DateUtil.dart';
+
+class MenuDate {
+  DateTime _time;
+
+  MenuDate([this._time]) {
+    if (this._time == null) {
+      this._time = DateTime.now();
+    }
+  }
+
+  forward() {
+    _time.add(Duration(days: 1));
+  }
+
+  back() {
+    _time.subtract(Duration(days: 1));
+    return this;
+  }
+
+  now() {
+    _time = DateTime.now();
+  }
+
+  static DiningHallHours getMostRelevant(Map<String, List<DiningHallHours>> hours, DateTime start) {
+    for (int i = 0; i < 7; ++i) {
+      List<DiningHallHours> hoursOnDay = hours[DateUtil.getWeekday(start).toLowerCase()];
+
+      DiningHallHours found;
+      if (i == 0) {
+        TimeOfDay timeOfDay = TimeOfDay.fromDateTime(start);
+
+        found = hoursOnDay.firstWhere((hours) {
+          if (hours.closed) {
+            return false;
+          }
+
+          // If the end time has already passed, get rid of it
+          if (timeOfDay.hour < hours.endTime.hour) {
+            return false;
+          }
+
+          if (timeOfDay.hour == hours.endTime.hour
+              && timeOfDay.minute < hours.endTime.minute) {
+            return false;
+          }
+
+          return true;
+        }, orElse: () => null);
+      } else {
+        found = hoursOnDay.firstWhere((hours) => !hours.closed, orElse: () => null);
+      }
+
+      if (found != null) {
+        return found;
+      }
+
+      start.add(Duration(days: 1));
+    }
+
+    return null;
+  }
+
+  String getFormatted() {
+    return new DateFormat("yyy-MM-dd").format(_time);
+  }
+}
