3d0f2ad852a2a7ff8865457576bd0d3001a9ff0e
Spencer <spencer@frozor.io>
2018-04-22 00:03:01 -0400

    change the logic behind request to use the http library

--- lib/api/request.dart
+++ lib/api/request.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+import 'package:http/http.dart' as http;
 
 Future toUTF8(HttpClientResponse res) async {
   Stream stream = res.transform(UTF8.decoder);
@@ -16,44 +17,32 @@ Future toUTF8(HttpClientResponse res) async {
   return responseText;
 }
 
-Future<Map<String, dynamic>> makeRestRequest(String url) async {
-  HttpClient client = new HttpClient();
-
-  HttpClientRequest req;
+Future makeRestRequest(String url) async {
+  http.Response res;
   try {
-    req = (await client.getUrl(Uri.parse(url)));
+    res = await http.get(url);
   } catch (e) {
     return new Future.error(e);
   }
-
-  HttpClientResponse res;
-  try {
-    res = await req.close();
-  } catch (e) {
-    return new Future.error(e);
-  }
-
-  String responseText;
-  try {
-    responseText = await toUTF8(res);
-  } catch (e) {
-    return new Future.error(e);
-  }
-
-  dynamic decodedUnknown = new JsonDecoder().convert(responseText);
-
-  if (!(decodedUnknown is Map)) {
-    return new Future.error('Unable to decode response into a map.');
-  }
-
-  Map<String, dynamic> decoded = decodedUnknown;
 
   if (!res.statusCode.toString().startsWith('2')) {
-    return new Future.error('Request unsuccessful: ${decoded.containsKey('error') ? '(${res.statusCode}) ${decoded['error']}' : res.statusCode}');
+    return new Future.error('Request unsuccessful: ${res.statusCode}');
   }
 
-  if (decoded.containsKey('error')) {
-    return new Future.error('Request returned an error: ${decoded['error']}');
+  if (res.body.length == 0) {
+    return new Future.error('Empty response.');
+  }
+
+  dynamic decoded = new JsonDecoder().convert(res.body);
+
+  if (decoded == null) {
+    return new Future.error('Empty JSON response.');
+  }
+
+  if (decoded is Map) {
+    if ((decoded as Map<String, dynamic>).containsKey('error')) {
+      return new Future.error('Request returned an error: ${decoded['error']}');
+    }
   }
 
   return decoded;
