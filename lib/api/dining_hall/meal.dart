adc96e79fb26162230354d1de4aad0d5cb7c36a9
Spencer <spencer@frozor.io>
2018-04-27 20:49:56 -0400

    add meal ordinals

--- lib/api/dining_hall/meal.dart
+++ lib/api/dining_hall/meal.dart
@@ -1,8 +1,8 @@
 class Meal {
-  static const Meal breakfast = Meal._internal(name: 'Breakfast', identifier: 192);
-  static const Meal lunch = Meal._internal(name: 'Lunch', identifier: 190);
-  static const Meal dinner = Meal._internal(name: 'Dinner', identifier: 191);
-  static const Meal lateNight = Meal._internal(name: 'Late Night', identifier: 232);
+  static const Meal breakfast = Meal._internal(name: 'Breakfast', identifier: 192, ordinal: 0);
+  static const Meal lunch = Meal._internal(name: 'Lunch', identifier: 190, ordinal: 1);
+  static const Meal dinner = Meal._internal(name: 'Dinner', identifier: 191, ordinal: 2);
+  static const Meal lateNight = Meal._internal(name: 'Late Night', identifier: 232, ordinal: 3);
 
   static final List<Meal> meal = new List.unmodifiable([
     Meal.breakfast, Meal.lunch, Meal.dinner, Meal.lateNight
