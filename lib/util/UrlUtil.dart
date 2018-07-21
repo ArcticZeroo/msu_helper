a370ff8355aae208f8d5f3302835c58fde0ab547
Spencer <spencer@frozor.io>
2018-07-21 11:56:06 -0400

    Rename methods in UrlUtil for clarity

--- lib/util/UrlUtil.dart
+++ lib/util/UrlUtil.dart
@@ -1,27 +1,27 @@
 import 'package:url_launcher/url_launcher.dart';
 
 class UrlUtil {
-  static open(String url, [String backup]) async {
+  static openUrl(String url, [String backup]) async {
     if (await canLaunch(url)) {
       await launch(url);
     } else {
       if (backup != null) {
-        return open(backup);
+        return openUrl(backup);
       } else {
         throw 'Could not open url $url';
       }
     }
   }
 
-  static openMaps(urlEnding) {
-    return open('https://maps.google.com/$urlEnding');
+  static openMapsCustom(urlEnding) {
+    return openUrl('https://maps.google.com/$urlEnding');
   }
 
   static openMapsToLocation(loc) {
-    return openMaps('?q=$loc');
+    return openMapsCustom('?q=$loc');
   }
 
   static openMapsToCoordinates(double x, double y) {
-    return openMaps('?q=$x,$y');
+    return openMapsCustom('?q=$x,$y');
   }
 }
