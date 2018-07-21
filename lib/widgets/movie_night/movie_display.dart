eee8537b8d880b764425a3777656e69044aecf8f
Spencer <spencer@frozor.io>
2018-07-20 21:50:23 -0400

    Styling for movie list entries

--- lib/widgets/movie_night/movie_display.dart
+++ lib/widgets/movie_night/movie_display.dart
@@ -82,27 +82,31 @@ class MovieDisplayState extends State<MovieDisplay> {
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialCard(
-        title: new InkWell(
-            child: new Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                new Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    new Text(widget.movie.title, style: MaterialCard.titleStyle),
-                    new Text('${widget.movie.showings.length} showing${widget.movie.showings.length == 1 ? '' : 's'} listed')
-                  ],
-                ),
-                new IconButton(
-                    icon: new Icon(collapsed ? Icons.keyboard_arrow_down : Icons.keyboard_arrow_up, color: Colors.black38, size: 24.0),
-                    onPressed: toggleCollapsed
-                )
-              ],
-            ),
-            onTap: toggleCollapsed
-        ),
-        body: collapsed ? null : buildBody()
+    return new Container(
+      padding: const EdgeInsets.all(8.0),
+      child: new MaterialCard(
+          backgroundColor: Colors.green[300],
+          title: new InkWell(
+              child: new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  new Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      new Text(widget.movie.title, style: MaterialCard.titleStyle),
+                      new Text('${widget.movie.showings.length} showing${widget.movie.showings.length == 1 ? '' : 's'} listed')
+                    ],
+                  ),
+                  new IconButton(
+                      icon: new Icon(collapsed ? Icons.keyboard_arrow_down : Icons.keyboard_arrow_up, color: Colors.black38, size: 24.0),
+                      onPressed: toggleCollapsed
+                  )
+                ],
+              ),
+              onTap: toggleCollapsed
+          ),
+          body: collapsed ? null : buildBody()
+      ),
     );
   }
 }
