7a074787a1d0d77768f250225e5749e562cf2010
Spencer <spencer@frozor.io>
2018-08-01 21:18:05 -0400

    Throw instead of return errored futures in makeRestRequest

--- lib/api/request.dart
+++ lib/api/request.dart
@@ -4,33 +4,29 @@ import 'package:http/http.dart' as http;
 
 Future makeRestRequest(String url, [Duration timeout = const Duration(seconds: 10)]) async {
   http.Response res;
-  try {
-    if (timeout != null) {
-      res = await http.get(url).timeout(timeout);
-    } else {
-      res = await http.get(url);
-    }
-  } catch (e) {
-    return new Future.error(e);
+  if (timeout != null) {
+    res = await http.get(url).timeout(timeout);
+  } else {
+    res = await http.get(url);
   }
 
   if (!res.statusCode.toString().startsWith('2')) {
-    return new Future.error('Request to $url unsuccessful: ${res.statusCode}');
+    throw new Exception('Request to $url unsuccessful: ${res.statusCode}');
   }
 
   if (res.body.length == 0) {
-    return new Future.error('Empty response.');
+    throw new Exception('Empty response.');
   }
 
   dynamic decoded = json.decode(res.body);
 
   if (decoded == null) {
-    return new Future.error('Empty JSON response.');
+    throw new Exception('Empty JSON response.');
   }
 
   if (decoded is Map) {
     if ((decoded as Map<String, dynamic>).containsKey('error')) {
-      return new Future.error('Request returned an error: ${decoded['error']}');
+      throw new Exception('Request returned an error: ${decoded['error']}');
     }
   }
 
