4284a8a0d1cf1fd2af2fec23a83783ad89ab9e63
Spencer <spencer@frozor.io>
2018-11-06 06:04:03 -0500

    Add ability for json provider to do all the work at once

--- lib/api/json_cache/provider.dart
+++ lib/api/json_cache/provider.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:meta/meta.dart';
 import 'package:msu_helper/api/database.dart';
+import 'package:msu_helper/api/request.dart';
 import 'package:msu_helper/api/timed_cache.dart';
 import 'package:msu_helper/config/expire_time.dart';
 import 'package:sqflite/sqflite.dart';
@@ -12,57 +14,97 @@ Map<String, TimedCacheEntry<String>> jsonCache = new Map();
 Lock jsonLock = new Lock();
 
 Future retrieveJsonFromDb(String key, [num expireTime = ExpireTime.THIRTY_MINUTES]) async {
-  return jsonLock.synchronized(() async {
-    Database db = await MainDatabase.getDbInstance();
+    return jsonLock.synchronized(() async {
+        Database db = await MainDatabase.getDbInstance();
 
-    List<Map<String, dynamic>> rows = await db.query(TableName.jsonCache,
-        where: 'name = ?', whereArgs: [key]);
+        List<Map<String, dynamic>> rows = await db.query(TableName.jsonCache,
+            where: 'name = ?', whereArgs: [key]);
 
-    if (rows.length == 0) {
-      return null;
-    }
+        if (rows.length == 0) {
+            return null;
+        }
 
-    Map<String, dynamic> row = rows[0];
+        Map<String, dynamic> row = rows[0];
 
-    if (!row.containsKey('json')) {
-      return null;
-    }
+        if (!row.containsKey('json')) {
+            return null;
+        }
 
-    int retrieved = row['retrieved'] as int;
+        int retrieved = row['retrieved'] as int;
 
-    if (!ExpireTime.isValid(retrieved, expireTime)) {
-      await db.delete(TableName.jsonCache,
-          where: 'name = ?', whereArgs: [key]);
-      return null;
-    }
+        if (!ExpireTime.isValid(retrieved, expireTime)) {
+            await db.delete(TableName.jsonCache,
+                where: 'name = ?', whereArgs: [key]);
+            return null;
+        }
 
-    return json.decode(row['json']);
-  });
+        return json.decode(row['json']);
+    });
 }
 
 Future saveJsonToDb(String key, dynamic object) async {
-  if (object == null) {
-    throw new Exception('Object to save to $key is null');
-  }
+    if (object == null) {
+        throw new Exception('Object to save to $key is null');
+    }
 
-  if (object is! String) {
-    object = json.encode(object);
-  }
+    if (object is! String) {
+        object = json.encode(object);
+    }
 
-  Database db = await MainDatabase.getDbInstance();
+    Database db = await MainDatabase.getDbInstance();
 
-  int updated = await db.update(TableName.jsonCache, {
-    'json': object,
-    'retrieved': DateTime.now().millisecondsSinceEpoch
-  }, where: 'name = ?', whereArgs: [key]);
+    int updated = await db.update(TableName.jsonCache, {
+        'json': object,
+        'retrieved': DateTime.now().millisecondsSinceEpoch
+    }, where: 'name = ?', whereArgs: [key]);
 
-  if (updated > 0) {
-    return;
-  }
+    if (updated > 0) {
+        return;
+    }
 
-  await db.insert(TableName.jsonCache, {
-    'json': object,
-    'retrieved': DateTime.now().millisecondsSinceEpoch,
-    'name': key
-  });
+    await db.insert(TableName.jsonCache, {
+        'json': object,
+        'retrieved': DateTime.now().millisecondsSinceEpoch,
+        'name': key
+    });
 }
+
+Future retrieveJsonFromWebAndSave(String key, String url, [Duration timeout = const Duration(milliseconds: 10*1000)]) async {
+    print('Getting json from web with url $url');
+    var data = await makeRestRequest(url).timeout(timeout);
+
+    print('Got it');
+
+    await saveJsonToDb(key, data);
+
+    return json.decode(data);
+}
+
+Future retrieveJson({
+    @required String key,
+    @required String urlIfNeeded,
+    Duration timeout = const Duration(seconds: 10),
+    num expireTime = ExpireTime.THIRTY_MINUTES
+}) async {
+    print('Getting json of key $key and fallback url $urlIfNeeded');
+
+    dynamic data = null;
+    try {
+        data = await retrieveJsonFromDb(key, expireTime);
+    } catch (e) {
+        print('Could not get json from db...');
+    }
+
+    if (data != null) {
+        if (data is! String) {
+            return data;
+        }
+
+        return json.decode(data);
+    }
+
+    print('Getting from web');
+
+    return retrieveJsonFromWebAndSave(key, urlIfNeeded, timeout);
+}
+
