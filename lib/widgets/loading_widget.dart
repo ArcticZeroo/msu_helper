1059edc8f7f51a1d9a334311ce4afbff7927c352
Spencer <spencer@frozor.io>
2018-12-14 20:07:22 -0500

    Fix infinite height of LoadingWidget and add optional centered

--- lib/widgets/loading_widget.dart
+++ lib/widgets/loading_widget.dart
@@ -2,30 +2,48 @@ import 'package:flutter/material.dart';
 
 class LoadingWidget extends StatelessWidget {
     final String name;
+    final bool centered;
 
-    const LoadingWidget({Key key, this.name}) : super(key: key);
+    const LoadingWidget({
+        Key key,
+        this.name,
+        this.centered = true
+    }) : super(key: key);
 
-    @override
-    Widget build(BuildContext context) => new Center(
-        child: new Container(
-            decoration: new BoxDecoration(
-                color: Theme.of(context).primaryColor,
-                borderRadius: const BorderRadius.all(Radius.circular(4.0))
-            ),
-            padding: const EdgeInsets.all(16.0),
-            child: new Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                    new SizedBox(
-                        width: 24.0,
-                        height: 24.0,
-                        child: new CircularProgressIndicator(valueColor: const AlwaysStoppedAnimation<Color>(Colors.white)),
+    Widget buildLoadingOnly(BuildContext context) => Container(
+        padding: const EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+          borderRadius: BorderRadius.circular(4.0)
+        ),
+        child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+                SizedBox(
+                  width: 24.0,
+                  height: 24.0,
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.white)
+                  )
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 8.0
+                  )
+                ),
+                Text(
+                    'Loading${name == null ? '' : ' $name'}...',
+                    style: TextStyle(
+                      color: Colors.white
                     ),
-                    new Container(width: 16.0),
-                    new Text('Loading${name != null ? ' $name' : ''}...', style: const TextStyle(color: Colors.white, fontSize: 16.0))
-                ],
-            ),
+                )
+            ],
         ),
     );
+
+    @override
+    Widget build(BuildContext context) => centered
+      ? Center(child: buildLoadingOnly(context))
+      : buildLoadingOnly(context);
 }
